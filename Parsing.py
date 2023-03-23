571f07c9aa698fc12b03db8c8a852424da16058e
filipp698 <fwolski.77@gmail.com>
2023-03-23 15:54:10 +0100

    Update reading data

--- Parsing.py
+++ Parsing.py
@@ -13,21 +13,39 @@ def importTL():
     with open("Dane\\dane_testlines.csv", encoding='utf-8-sig') as daneTL:
         reader = csv.DictReader(daneTL, delimiter=";")
         for line in reader:
+            bandsNR.append(line['Bandy5G'].split(','))
+            bandsLTE.append(line['Bandy4G'].split(','))
             nrTL.append(line['NrTL'])
-            bandsNR.append(line['Bandy5G'])
-            bandsLTE.append(line['Bandy4G'])
-            print(line['NrTL'] + " Bandy NR: " + line['Bandy5G']+ " Bandy LTE: " + line['Bandy4G'])
-        # print(nrTL)
+            #print(line['NrTL'] + " Bandy NR: " + line['Bandy5G']+ " Bandy LTE: " + line['Bandy4G'])
+        print(nrTL)
+        #print(bandsNR)
+        #print(bandsLTE)
+        #print(bandsNR[1])
+        #print(len(bandsNR))
+
+        for listNR in bandsNR:
+            for i in range(len(listNR)):
+                if listNR[i] in slownik:
+                    listNR[i] = slownik[listNR[i]]
         # print(bandsNR)
-        # print(bandsLTE)
+        # print(len(listNR))
+        # print(listNR)
+
+        for listLTE in bandsLTE:
+            for i in range(len(listLTE)):
+                if listLTE[i] in slownik:
+                    listLTE[i] = slownik[listLTE[i]]
+        print(bandsLTE[0])
+        # print(len(listNR))
+        # print(len(bandsNR))
+        # print(listLTE)
+
+#Poprawić wczytywanie danych
+    with open("Dane\\testlines.txt",'w',newline='') as file:
+        writer = csv.writer(file)
         for i in range(len(nrTL)):
-            for j in slownik:
-                if i == slownik[j]:
-                    bandsNR[i] = slownik[bandsNR[i]]
-                    bandsLTE[i] = slownik[bandsLTE[i]]
-        # print(nrTL)
-        # print(bandsNR)
-        # print(bandsLTE)
+            iloscRU = len(bandsNR[i] + bandsLTE[i])
+            writer.writerow([nrTL[i], iloscRU, bandsNR[i], bandsLTE[i]])
 
 def importRU():
     iloscRU = []
@@ -40,9 +58,12 @@ def importRU():
             #print("Band " + line['Band'] + " Ilosc radii: " + line['IloscRUs'])
     #print(bandy)
     for i in range(len(bandy)):
-        for j in slownik:
-            if i == slownik[j]:
-                bandy[i] = slownik[bandy[i]]
+        if bandy[i] in slownik:
+            bandy[i] = slownik[bandy[i]]
+    print(bandy)
+        # for j in slownik:
+        #     if i == slownik[j]:
+        #         bandy[i] = slownik[bandy[i]]
     with open("Dane\\radia.txt",'w',newline='') as file:
         writer = csv.writer(file)
         for k in range(len(bandy)):
@@ -64,10 +85,13 @@ def importOrder():
             print("Zamówienie nr: " + line['Nrzam'] + " Band 5G: " + line['Pasmo5G'] + " Band 4G: " + line['Pasmo4G'])
         #Porównanie wartości bandów z kluczem słownikowym i ich zamiana
         for i in range(len(nrZam)):
-            for j in slownik:
-                if i == slownik[j]:
-                    band5G[i] = slownik[band5G[i]]
-                    band4G[i] = slownik[band4G[i]]
+            if band5G[i] or band4G[i] in slownik:
+                band5G[i] = slownik[band5G[i]]
+                band4G[i] = slownik[band4G[i]]
+            # for j in slownik:
+            #     if i == slownik[j]:
+            #         band5G[i] = slownik[band5G[i]]
+            #         band4G[i] = slownik[band4G[i]]
 
         print("Po zamianie:", band5G)
         print("Po zamianie:", band4G)
